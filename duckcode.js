0d2df21a45b70008c23a8bb05c4ab7d8091f1ceb
99061142 <99061142@mydavinci.nl>
2020-12-19 19:54:15 +0100

    Fixing the images
    i fixed the image and i started with some js code

---
+++ duckcode.js
@@ -0,0 +1,35 @@
+//Variable voor de gif
+var duck = document.getElementById("duck");
+var marginVertical = 250;
+var marginHorizontal = 450
+var duckStyle;
+
+function fly(){
+var direction = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+}
+setTimeout(fly, 500)
+
+direction = "N"
+
+for (i = 0; i < 3; i++) {
+if(direction = "N"){
+	duckStyle = marginVertical - 75;
+	duck.style.marginTop = duckStyle + "px";
+	duckStyle;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
