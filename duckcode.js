0c317afd6b123b237a13ee3aa91b40211021396b
99061142 <99061142@mydavinci.nl>
2020-12-19 23:08:07 +0100

    Javascript
    Changed some codes.

--- duckcode.js
+++ duckcode.js
@@ -1,11 +1,16 @@
 var duck = document.getElementById("duck");  //Var voor de image
 var direction = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];  //Var voor de directie
+var background = document.getElementById("background");
+
+//Puntenteller
+var hitcounter = 0;
+var misscounter = 0;
+var totalscore = 0;
 
 
 //De functie pakt iedere halve seconde een directie 
 function randomdirection(){
 	var randomdirection = direction[Math.floor(Math.random() * direction.length)];
-	console.log(randomdirection)
 }
 setInterval(randomdirection, 500)
 
@@ -42,6 +47,13 @@ else if(randomdirection = "NW"){
 
 }
 
+
+duck.addEventListener("click", function(){
+	hitcounter = hitcounter + 1;
+});
+background.addEventListener("click", function(){
+	misscounter = misscounter + 1;
+});
 
 
 
